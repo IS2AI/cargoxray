75820479b058de00613edfcb0c5456466400618d
contactone <ruslan.bazhenov@nu.edu.kz>
2021-08-30 04:12:11 +0600

    Torch dataset

---
+++ cargoxray.py
@@ -0,0 +1,17 @@
+import torch.utils.data
+
+
+class CargoXRay(torch.utils.data.Dataset):
+
+    def __init__(self):
+        self.d = 1
+
+    def __getitem__():
+        return None
+
+    def __len__():
+        return 1
+
+
+if __name__ == '__main__':
+    ds = CargoXRay()
