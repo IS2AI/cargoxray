cd02310ee6d7788054f903d6eb72aeda7913f2fd
contactone <ruslan.bazhenov@nu.edu.kz>
2021-08-30 11:42:03 +0600

    Start to pandas in import

--- cargoxray.py
+++ cargoxray.py
@@ -1,16 +1,35 @@
 import torch.utils.data
-
+from pathlib import Path
+import json
+from typing import Union
 
 class CargoXRay(torch.utils.data.Dataset):
 
-    def __init__(self):
-        self.d = 1
+    def __init__(self,
+                 data_dir: Union[str, Path],
+                 transform=None):
+        with Path(data_dir) \
+                .joinpath('annotations.json') \
+                .open('r') as fs:
+            self.annotations = json.load(fs)
+        
+        annotations = []
+        for img in self.annotations:
+            for ann in img['regions']:
+                annotations.append({
+                    'image': img['image'],
+                    'region': ann,
+                })
+        self.annotations = annotations
 
-    def __getitem__():
-        return None
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        
 
-    def __len__():
-        return 1
+    def __len__(self):
+        return len(self.annotation)
 
 
 if __name__ == '__main__':
