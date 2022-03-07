5f80efa00bfe94593cce1c99b868bafb2610b4f7
loyaltytwo <ruslan.bazhenov@nu.edu.kz>
2022-03-07 21:00:17 +0600

    Metrics added to pipeline

--- src/model/pretrain.sh
+++ src/model/pretrain.sh
@@ -1,4 +1,1 @@
-python src/model/yolov5/train.py --weights /usr/src/basemodels/yolov5s6.pt
-      --data prepared_opix/dataset.yaml --hyp models/params/pretrain_hyp.yaml --epochs
-      1 --batch-size 64 --imgsz 1024 --device 0 --project models --name pretrained
-      --cache
+python src/model/yolov5/train.py --weights /usr/src/basemodels/yolov5s6.pt --data prepared_opix/dataset.yaml --hyp models/params/pretrain_hyp.yaml --epochs 1 --batch-size 64 --imgsz 1024 --device 0 --project models --name pretrained --cache
